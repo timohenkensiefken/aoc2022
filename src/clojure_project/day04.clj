b22e1939c8a1eef74de234c7c4c5b864e07a6325
Timo <timo.henkensiefken@uni-ulm.de>
2022-12-05 10:42:01 +0100

    day 04

---
+++ src/clojure_project/day04.clj
@@ -0,0 +1,43 @@
+(ns clojure-project.day03
+  (:use clojure-project.core))
+
+(defn int-subs [intervals]
+  (let [[[a0 a1] [b0 b1]] intervals]
+    (or
+     (and (>= a0 b0) (<= a1 b1))
+     (and (<= a0 b0) (>= a1 b1)))))
+
+(defn int-overlap [intervals]
+  (let [[[a0 a1] [b0 b1]] intervals]
+    (or
+     (<= a0 b0 a1)
+     (<= a0 b1 a1)
+     (<= b0 a0 b1)
+     (<= b0 a1 b1))))
+
+(defn parse-interval [si]
+  (->> (clojure.string/split si #"-")
+       (mapv #(Integer/parseInt %))))
+
+(defn func [lines]
+  (->> (map (fn [l]
+              (->> (clojure.string/split l #",")
+                   (mapv parse-interval)
+                   (int-subs)
+                   )) lines)
+       (filter identity)
+       (count)
+       ))
+
+(defn func2 [lines]
+  (->> (map (fn [l]
+              (->> (clojure.string/split l #",")
+                   (mapv parse-interval)
+                   (int-overlap)
+                   )) lines)
+       (filter identity)
+       (count)
+       ))
+
+(process-input "resources/day04" func)  ;; Part 1
+(process-input "resources/day04" func2) ;; Part 2
